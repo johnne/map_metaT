b7da08e146f7c204294b81ebd8dbe2bc25be963a
johnne <john.sundh@scilifelab.se>
2022-04-05 10:36:50 +0200

    Add function to clean featurecount output

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from collections import defaultdict
 
+
 def parse_samples(f):
     df = pd.read_csv(f, sep="\t", index_col=0)
     samples = defaultdict(lambda: defaultdict(dict))
@@ -10,7 +11,30 @@ def parse_samples(f):
         samples[sample_id]["fq2"] = d["fq2"]
     return samples
 
+
 def parse_assemblies(f, datadir="data/"):
     df = pd.read_csv(f, sep="\t", index_col=0)
     assemblies = df.to_dict(orient="index")
     return assemblies
+
+
+def clean_featurecount(sm):
+    import os
+    dataf = pd.DataFrame()
+    for f in sm.input.tsv:
+        sample = os.path.basename(f).replace(".fc.tsv", "")
+        df = pd.read_csv(f, sep="\t", comment="#", usecols=[0, 1, 6])
+        df.columns = ["Geneid", "Chr", sample]
+        df.index = df.Chr.map(str) + ["_" + x.split("_")[-1] for x in df.Geneid]
+        df.drop(["Geneid","Chr"], axis=1, inplace=True)
+        dataf = pd.merge(dataf, df, left_index=True, right_index=True, how="outer")
+    dataf.to_csv(sm.output.tsv, sep="\t")
+
+
+def main(sm):
+    toolbox = {"clean_featurecount": clean_featurecount}
+    toolbox[sm.rule](sm)
+
+
+if __name__ == "__main__":
+    main(snakemake)
