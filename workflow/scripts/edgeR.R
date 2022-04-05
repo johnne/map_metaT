5c6943a10ada1f0d5db3960e3305f489e5d8963a
johnne <john.sundh@scilifelab.se>
2022-04-05 22:53:04 +0200

    Add functionality for rpkm calculations

---
+++ workflow/scripts/edgeR.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+source("workflow/scripts/common.R")
+
+library(edgeR)
+method <- snakemake@params$method
+input <- snakemake@input[[1]]
+output <- snakemake@output[[1]]
+if (is.null(snakemake@wildcards$db)) {
+    db = ""
+} else {
+    db <- snakemake@wildcards$db
+}
+print(db)
+
+# Read the counts
+if (db == "modules") {
+    x <- read.delim(input, sep = "\t", header = TRUE)
+    rownames(x) <- x$module
+    x <- subset(x, select=-c(module))
+} else {
+    x <- read.delim(input, row.names = 1, sep = "\t", header = TRUE)
+}
+# Get sample names
+sample_names <- colnames(x)[unlist(lapply(x, is.numeric))]
+# Extract row names
+xrownames <- row.names(x)[row.names(x)!="Unclassified"]
+# Get info names
+info_names <- colnames(x)[unlist(lapply(x, is.character))]
+# Remove unclassified
+if ("Unclassified" %in% row.names(x)){
+    x <- x[row.names(x)!="Unclassified", ]
+}
+
+x <- as.data.frame(x, row.names=xrownames)
+colnames(x) <- append(info_names, sample_names)
+
+x_num <- process_data(x, output)
+
+if (method %in% c("TMM", "RLE")) {
+    # Create DGE
+    obj <- DGEList(x_num)
+    # Calculate norm factors
+    obj <- calcNormFactors(obj, method = method)
+    # Calculate cpms
+    norm <- cpm(obj, normalized.lib.sizes = TRUE)
+} else if (method == "RPKM") {
+    # Extract gene length column
+    gene_length <- x_num$Length
+    x_num <- x_num[, colnames(x_num) != "Length"]
+    obj <- DGEList(x_num)
+    # Calculate norm factors
+    obj <- calcNormFactors(obj, method = "TMM")
+    # Calculate RPKM
+    norm <- rpkm(obj, normalized.lib.sizes = TRUE, gene.length = gene_length)
+}
+
+# Add info columns back
+norm <- cbind(str_cols(x), norm)
+if (length(info_names)>0) {
+    colnames(norm) <- append(info_names, sample_names)
+}
+# Convert to numeric
+norm <- as.data.frame(norm)
+row.names(norm) <- xrownames
+
+write.table(x = norm, file = output, quote = FALSE, sep="\t")
