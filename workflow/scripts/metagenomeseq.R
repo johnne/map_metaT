29fd58bc242e82d06464c60b4c2eeaf6dcffa0fb
John Sundh <john.sundh@scilifelab.se>
2023-01-06 10:26:56 +0100

    Update R scripts

--- workflow/scripts/metagenomeseq.R
+++ workflow/scripts/metagenomeseq.R
@@ -16,8 +16,8 @@ db <- snakemake@wildcards$db
 #Read the counts
 if (db == "modules") {
         x <- read.delim(input, sep = "\t", header = TRUE, check.names = FALSE)
-    rownames(x) <- x$modules
-        x <- subset(x, select=-c(modules))
+    rownames(x) <- x$module
+        x <- subset(x, select=-c(module))
 } else {
         x <- read.delim(input, row.names = 1, sep = "\t", header = TRUE, check.names = FALSE)
 }
